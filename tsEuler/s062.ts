12374bb2c9dd07dee433c3ff82c0463ba349d574
scottoman <73678037+scottoman@users.noreply.github.com>
2023-04-05 00:06:48 -0500

    made s062.ts better

--- tsEuler/s062.ts
+++ tsEuler/s062.ts
@@ -6,30 +6,30 @@ const cube = (n : number) => {
     return n ** 3;
 }
 
-const genCubes = (n : number) => {
-    const res = new Array<string>();
+const genCubeMap = (n : number) => {
+    const cmap = new Map<string, Set<number>>();
     for(let i = 1; i <= n; i++){
-        res.push(cube(i).toString());
-    }
-    return res;
-}
-
-const checkCubes = (carr : Array<string>) => {
-    for(let i = 0; i < carr.length; i++){
-        const curr = new Set<string>();
-        for(let j = i; j < carr.length; j++){
-            if(carr[i].length != carr[j].length){
-                break;
-            }
-            if(sortNum(carr[i]) == sortNum(carr[j])){
-                curr.add(carr[i]);
-                curr.add(carr[j]);
-            }
-            if(curr.size == 5){
-                return curr;
-            }
+        const curr = cube(i);
+        const sorted = sortNum(curr.toString());
+        if(cmap.has(sorted)){
+            const currSet = cmap.get(sorted)!;
+            currSet.add(curr);
+            cmap.set(sorted, currSet);
+        }else{
+            const currSet = new Set<number>();
+            currSet.add(curr);
+            cmap.set(sorted, currSet); 
         }
     }
+    const sets = new Set<Set<number>>();
+    cmap.forEach((value : Set<number>) => {
+        if(value.size == 5) sets.add(value);
+    });
+    const mins = new Set<number>();
+    sets.forEach((s : Set<number>) => {
+        mins.add(Math.min(...s));
+    });
+    console.log(Math.min(...mins));
 }
 
-console.log(checkCubes(genCubes(10000)));
+genCubeMap(10000);
