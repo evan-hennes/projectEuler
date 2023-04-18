9c5fbd3a885376065b78798b2daafe9564afee48
scottoman <73678037+scottoman@users.noreply.github.com>
2023-04-18 01:20:27 -0500

    just need to check connections

--- tsEuler/s186.ts
+++ tsEuler/s186.ts
@@ -1,38 +1,32 @@
 const calls = new Map<number, Set<number>>();
 const pmNum = 524287;
+const connections = new Set<number>();
+let numConnected = 0;
+let numSuccessful = 0;
 
 const memoize = <P,R>(fn: (param: P) => R): ((param: P) => R) => {
     const cache = new Map<P, R>();
-    return (p : P) => {
-        if(cache.get(p) === undefined){
-            cache.set(p, fn(p));
-            return cache.get(p)!;
-        }else{
-            return cache.get(p)!;
-        }
-    }
+    return (p : P) => {return(!cache.has(p) ? cache.set(p, fn(p)).get(p)! : cache.get(p)!);};
 }
 
 const lfib = memoize((k : number) : number => {
-    if(1 <= k && k <= 55){
-        return (100003 - (200003 * k) + (300007 * (k ** 3))) % 1000000;
-    }else{
-        return (lfib(k - 24) + lfib(k - 55)) % 1000000;
-    }
+    if(1 <= k && k <= 55) return (100003 - (200003 * k) + (300007 * (k ** 3))) % 1000000;
+    return (lfib(k - 24) + lfib(k - 55)) % 1000000;
 });
 
-const checkConnection = (conn : number, n : number) => {
-    
+const checkConnections = (n : number) => {
+    return n;
 }
 
-let numConnected = 0;
-let numSuccessful = 0;
-for(let i = 1; i <= 1000000; i++){
+let i = 1;
+let pmCaller = 0;
+while(numConnected / numSuccessful != 0.99){
     const caller = lfib((2 * i) - 1);
     const called = lfib(2 * i);
     if(caller === called){
         console.log('misdial');
     }else{
+        numSuccessful++;
         if(calls.has(caller)){
             const arr = calls.get(caller)!.add(called);
             calls.set(caller, arr);
@@ -41,6 +35,17 @@ for(let i = 1; i <= 1000000; i++){
             arr.add(called);
             calls.set(caller, arr);
         }
+        if(caller === pmNum || called === pmNum){
+            console.log('direct connection');
+            pmCaller = caller;
+            break;
+        }
+    }
+    i++;
+    if(pmCaller != 0){
+        console.log(i);
+        break;
     }
 }
-console.log('done');
+checkConnections(pmCaller);
+console.log(connections.size / numSuccessful);
