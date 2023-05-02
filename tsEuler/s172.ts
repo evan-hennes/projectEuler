f6df46f0ccaabb470d073be169a838324cc97d50
scottoman <73678037+scottoman@users.noreply.github.com>
2023-05-01 21:51:53 -0500

    finished s172.ts

--- tsEuler/s172.ts
+++ tsEuler/s172.ts
@@ -1,13 +1,14 @@
 import { memoize } from "./useful.ts";
 
 const startingNums = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 18];
+const zeroes = [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 17];
 
 const findNums = memoize((nums : Array<number>) : number => {
     let sum = 0;
-    if(nums[10] == 0){
+    if(nums[10] === 0){
         return 1;
     }
-    for(let i = 0; i < 9; i++){
+    for(let i = 0; i <= 9; i++){
         const curr = [...nums];
         if(curr[i] === 3){
             continue;
@@ -19,4 +20,4 @@ const findNums = memoize((nums : Array<number>) : number => {
     return sum;
 });
 
-console.log(findNums(startingNums));
+console.log(findNums(startingNums) - findNums(zeroes));
